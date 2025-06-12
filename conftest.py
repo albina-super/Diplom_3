ec2ea02455e2830896ddd105bc44c5dac304d256
Toktieva Albina <albinawork777@gmail.com>
2025-06-12 14:13:12 +0500

    Добавила фикстуры где создается юзер через апи а также methods для апи методов

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from selenium import webdriver
 import data
-from data import MAIN_PAGE_URL
+from data import MAIN_PAGE_URL, MY_USER_DATA
+from methods.user_methods import UserMethods
+
 
 @pytest.fixture(params=['chrome', 'firefox'])
 def driver(request):
@@ -16,3 +18,13 @@ def driver(request):
     yield driver
 
     driver.quit()
+
+
+
+@pytest.fixture
+def user():
+    user = UserMethods()
+    user.register_user(MY_USER_DATA)
+    yield MY_USER_DATA
+    status, response_login = user.login_user(MY_USER_DATA)
+    user.delete_user(response_login['accessToken'])
--- data.py
+++ data.py
@@ -1,11 +1,17 @@
 
-USER_DATA = {
-    'email': 'albina_toktieva_20_777@gmail.com',
-    'password': 'Pass123&',
+MY_USER_DATA = {
+    "email": "my_user555@test.com",
+    "password": "test123",
+    "name": "my_user"
 }
-
 
 MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
 ORDER_HISTORY_URL = 'account/order-history'
 
 BROWSER_NAME = ''
+
+
+BASE_URL = ' https://stellarburgers.nomoreparties.site/api/'
+REGISTER_USER_URL = 'auth/register'
+USER_URL = 'auth/user'
+LOGIN_USER_URL = 'auth/login'
